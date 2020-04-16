4c28a61d2fe3db6bac9b5654fc139f497e6d1212
Daniel Vo <daniiielvo@gmail.com>
2020-04-16 18:51:37 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,10 @@
-def unsafe?(speed)
-
-end
-
-
-
-def not_safe?(speed)
-	
-end
-	
-
-
+def unsafe? (speed)
+ if speed <= 40 
+   return "true"
+ elsif speed >=60
+   return "true"
+ else
+   return "false"
+   
+ end 
+ end
